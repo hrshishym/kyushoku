c5a27d7dea1e5d3eebf11236a0be3e90a9d165ca
hrshishym <chappy.dogheart@gmail.com>
2025-06-22 03:12:54 +0900

    レイアウトファイル： noindex,nofollow追加

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="robots" content="noindex,nofollow">
     <title>給食献立管理</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
